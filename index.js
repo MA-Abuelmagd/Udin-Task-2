21de7b42fdc09acbd1899f54bb6bc3ceb31d1314
Mohamed Ayman <mohamed.ayman7319@gmail.com>
2024-09-11 14:02:53 +0300

    Get Specific Product API
    added the get request of finding a specific product with the custom ID in index.js file

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/',(req,res)=>{
     res.send("Hello Udin Technical Test")
 });
 
-app.get('/getAll',async (req,res)=>{
+app.get('/getall',async (req,res)=>{
     try{
         const products = await Product.find({});
         res.status(200).json(products);
@@ -31,6 +31,22 @@ app.get('/getAll',async (req,res)=>{
     }
 })
 
+app.get('/getproduct/:id', async (req, res) => {
+    try {
+      const id = req.params.id;
+      const product = await Product.findOne({id});
+  
+      if (!product) {
+        console.log("Product not found!");
+        res.status(404).json({ message: "Product not found! Make sure you entered the ID correctly" });
+      } else {
+        res.status(200).json(product);
+      }
+    } catch (error) {
+      console.error("Error fetching product:", error);
+      res.status(500).json({ message: "Error fetching product" });
+    }
+  });
 
 
 app.post('/createProduct', async (req,res)=>{
