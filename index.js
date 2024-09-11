5426a12744b12d54c01a6ba593eebe859b9fbcfd
Mohamed Ayman <mohamed.ayman7319@gmail.com>
2024-09-11 12:38:23 +0300

    Created the Product Model
    created the product schema and exported it

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const app = express()
+const Product = require('./models/product.model.js');
+
+app.use(express.json())
 
 app.listen(3000, () =>{
     console.log("Server is running on port 3000");
 });
-
-app.get('/',(req,res)=>{
-    res.send("Hello Udin Technical Test")
-});
-
 mongoose.connect("mongodb+srv://admin:admin1password@backenddb.sqkks.mongodb.net/Udin-API?retryWrites=true&w=majority&appName=BackendDB")
 .then(()=>{
     console.log("Connected to database!")
@@ -17,3 +15,14 @@ mongoose.connect("mongodb+srv://admin:admin1password@backenddb.sqkks.mongodb.net
 .catch((err)=>{
     console.log(err)
 });
+
+
+
+app.get('/',(req,res)=>{
+    res.send("Hello Udin Technical Test")
+});
+
+
+
+
+
---
+++ models/product.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema(
+    {
+        id:{
+            type: String,
+            required: [true,"Please enter the ID of the product"],
+            unique: true,
+            index: true
+        },
+        name:{
+            type: String,
+            required: [true, "Please enter the name of the product"]
+        },
+        description:{
+            type: String,
+            required: false
+        },
+        price:{
+            type:String,
+            required: [true, "Please enter the price"]
+        },
+        quantity:{
+            type: String,
+            required: true,
+            default: 0
+        }
+    }
+);
+
+const Product = mongoose.model("Product",ProductSchema);
+
+module.exports = Product;
